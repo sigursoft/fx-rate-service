8c043edc46bd1c8d77e704ebd316d10b1de844a8
Anton Kozik <pazitron@gmail.com>
2023-10-09 19:16:53 +0200

    add logging on save

--- src/main/java/com/sigursoft/fxrateservice/entity/ExchangeRateEntity.java
+++ src/main/java/com/sigursoft/fxrateservice/entity/ExchangeRateEntity.java
@@ -9,9 +9,34 @@ import java.time.Instant;
 
 @Table("fx_rate")
 public record ExchangeRateEntity(
-		@Id @Column("id") Integer id,
+		@Id @Column("id") Long id,
 		@Column("buy_currency") String buyCurrency,
 		@Column("sell_currency") String sellCurrency,
 		@Column("rate") BigDecimal rate,
 		@Column("created_at") Instant createdAt
-){}
+){
+	public static final class Builder {
+		String buyCurrency;
+		String sellCurrency;
+		BigDecimal rate;
+
+		public Builder buyCurrency(String buyCurrency) {
+			this.buyCurrency = buyCurrency;
+			return this;
+		}
+
+		public Builder sellCurrency(String sellCurrency) {
+			this.sellCurrency = sellCurrency;
+			return this;
+		}
+
+		public Builder rate(BigDecimal rate) {
+			this.rate = rate;
+			return this;
+		}
+
+		public ExchangeRateEntity build() {
+			return new ExchangeRateEntity(null, buyCurrency, sellCurrency, rate, null);
+		}
+	}
+}
--- src/main/java/com/sigursoft/fxrateservice/service/ExchangeRateService.java
+++ src/main/java/com/sigursoft/fxrateservice/service/ExchangeRateService.java
@@ -3,6 +3,8 @@ package com.sigursoft.fxrateservice.service;
 import com.sigursoft.fxrateservice.domain.ExchangeRate;
 import com.sigursoft.fxrateservice.entity.ExchangeRateEntity;
 import com.sigursoft.fxrateservice.repository.ExchangeRateRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -11,12 +13,19 @@ import reactor.core.publisher.Mono;
 @Service
 public class ExchangeRateService {
 
+	private static final Logger logger = LoggerFactory.getLogger(ExchangeRateService.class);
+
 	@Autowired
 	private ExchangeRateRepository exchangeRateRepository;
 
-	public Mono<ExchangeRateEntity> save(ExchangeRate exchangeRate) {
-		var entity = new ExchangeRateEntity(null, exchangeRate.buyCurrency(), exchangeRate.sellCurrency(), exchangeRate.rate(), null);
-		return exchangeRateRepository.save(entity);
+	public void save(ExchangeRate exchangeRate) {
+		exchangeRateRepository.save(
+				new ExchangeRateEntity.Builder()
+						.buyCurrency(exchangeRate.buyCurrency())
+						.sellCurrency(exchangeRate.sellCurrency())
+						.rate(exchangeRate.rate())
+						.build()
+		).doOnSuccess(e -> logger.info("Saved exchange rate " + e.toString())).subscribe();
 	}
 
 	public Flux<ExchangeRateEntity> provideExchangeRates() {
