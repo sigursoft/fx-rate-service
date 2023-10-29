1bf9738a8345eb8f307ea6475938a4ec73c55b96
Anton Kozik <pazitron@gmail.com>
2023-10-29 17:01:30 +0100

    add repository test

--- src/main/java/com/sigursoft/fxrateservice/repository/ExchangeRateRepository.java
+++ src/main/java/com/sigursoft/fxrateservice/repository/ExchangeRateRepository.java
@@ -4,7 +4,7 @@ import com.sigursoft.fxrateservice.entity.ExchangeRateEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
-public interface ExchangeRateRepository extends ReactiveCrudRepository<ExchangeRateEntity, Integer> {
+public interface ExchangeRateRepository extends ReactiveCrudRepository<ExchangeRateEntity, Long> {
 
 	Mono<ExchangeRateEntity> findFirstByBuyCurrencyAndSellCurrencyOrderByCreatedAtDesc(String buyCurrency, String sellCurrency);
 }
---
+++ src/test/java/com/sigursoft/fxrateservice/repository/ExchangeRateRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.sigursoft.fxrateservice.repository;
+
+import com.sigursoft.fxrateservice.entity.ExchangeRateEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+@Testcontainers
+class ExchangeRateRepositoryTest {
+
+	@SuppressWarnings("resource")
+	@Container
+	@ServiceConnection
+	static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:10.11.5")
+			.withDatabaseName("fx")
+			.withUsername("user")
+			.withPassword("password")
+			.withCopyFileToContainer(
+					MountableFile.forClasspathResource("db/schema.sql"),
+					"/docker-entrypoint-initdb.d/schema.sql"
+			);
+
+	@Autowired
+	ExchangeRateRepository exchangeRateRepository;
+
+	@Test
+	void findFirstByBuyCurrencyAndSellCurrencyOrderByCreatedAtDesc() {
+		// given
+		var exchangeRateEntity = new ExchangeRateEntity.Builder()
+				.buyCurrency("USD")
+				.sellCurrency("EUR")
+				.rate(new BigDecimal("0.9341"))
+				.build();
+		var persistedExchangeRateEntity = exchangeRateRepository.save(exchangeRateEntity).block();
+		assert persistedExchangeRateEntity != null;
+		var persistedExchangeRateEntityWithTimestamp = exchangeRateRepository.findById(persistedExchangeRateEntity.id()).block();
+		// when
+		ExchangeRateEntity result = exchangeRateRepository.findFirstByBuyCurrencyAndSellCurrencyOrderByCreatedAtDesc("USD", "EUR").block();
+		// then
+		assert result != null;
+		assert persistedExchangeRateEntityWithTimestamp != null;
+		Assertions.assertEquals(persistedExchangeRateEntityWithTimestamp, result);
+	}
+}
