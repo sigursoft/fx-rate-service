71db97608da0119650665521f7143f079413d0bd
Anton Kozik <pazitron@gmail.com>
2023-10-30 23:03:31 +0100

    use Java 21 Virtual Threads

--- src/main/java/com/sigursoft/fxrateservice/configuration/SchedulingConfiguration.java
+++ src/main/java/com/sigursoft/fxrateservice/configuration/SchedulingConfiguration.java
@@ -1,35 +1,29 @@
 package com.sigursoft.fxrateservice.configuration;
 
 import com.sigursoft.fxrateservice.tasks.ExchangeRateScheduledTask;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.List;
+
 @Configuration
 @EnableScheduling
 public class SchedulingConfiguration {
 
+	@Value("${fx.currencies}")
+	private List<String> currencies;
+
 	@Bean
-	@ConditionalOnProperty(value = "tasks.enabled", matchIfMissing = true, havingValue = "true")
-	public ExchangeRateScheduledTask usdRateScheduledTask() {
+	@ConditionalOnProperty(value = "fx.tasks.enabled", matchIfMissing = true, havingValue = "true")
+	public ExchangeRateScheduledTask updateRatesScheduledTask() {
 		return new ExchangeRateScheduledTask() {
 
 			@Override
-			protected String currency() {
-				return "USD";
-			}
-		};
-	}
-
-	@Bean
-	@ConditionalOnProperty(value = "tasks.enabled", matchIfMissing = true, havingValue = "true")
-	public ExchangeRateScheduledTask euroRateScheduledTask() {
-		return new ExchangeRateScheduledTask() {
-
-			@Override
-			protected String currency() {
-				return "EUR";
+			protected List<String> currencies() {
+				return currencies;
 			}
 		};
 	}
--- src/main/java/com/sigursoft/fxrateservice/tasks/ExchangeRateScheduledTask.java
+++ src/main/java/com/sigursoft/fxrateservice/tasks/ExchangeRateScheduledTask.java
@@ -9,9 +9,17 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Component
 public abstract class ExchangeRateScheduledTask {
 	private static final Logger logger = LoggerFactory.getLogger(ExchangeRateScheduledTask.class);
+
+	private static final String PLN = "PLN";
+
 
 	@Autowired
 	private WebClient polygonGatewayClient;
@@ -19,12 +27,22 @@ public abstract class ExchangeRateScheduledTask {
 	@Autowired
 	private ExchangeRateService exchangeRateService;
 
-	protected abstract String currency();
+	protected abstract List<String> currencies();
 
 	@Scheduled(cron = "0 * * * * *")
-	public void fetchExchangeRate() {
-		logger.info("Fetching rate for " + currency());
-		polygonGatewayClient.get().uri("/exchange-rate/" + currency() +"/PLN").retrieve()
+	public void fetchExchangeRates() {
+		var sellSide = currencies().stream().map(currency -> new String[] {currency, PLN}).toList();
+		var buySide = currencies().stream().map(currency -> new String[] {PLN, currency}).toList();
+		var all = new ArrayList<>(sellSide);
+		all.addAll(buySide);
+		try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+			all.forEach(currencyPair -> executorService.submit(() -> fetchExchangeRate(currencyPair[0], currencyPair[1])));
+		}
+	}
+
+	private void fetchExchangeRate(String buyCurrency, String sellCurrency) {
+		logger.info("Fetching rate for " + buyCurrency + ":" + sellCurrency);
+		polygonGatewayClient.get().uri("/exchange-rate/" + buyCurrency +"/" + sellCurrency).retrieve()
 				.bodyToMono(ExchangeRate.class).subscribe(exchangeRateService::save);
 	}
 }
